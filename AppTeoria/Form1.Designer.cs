f2e55a62187952c0d13d89865e666c236c9448a1
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-03-06 22:14:55 -0300

    AppTeoria
    Aula dia 06/03/2026

---
+++ AppTeoria/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AppTeoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.cbbExrcicios = new System.Windows.Forms.ComboBox();
+            this.btnIF = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // cbbExrcicios
+            // 
+            this.cbbExrcicios.FormattingEnabled = true;
+            this.cbbExrcicios.Items.AddRange(new object[] {
+            "LEGPRESS",
+            "ADUTORA",
+            "ABDUTORA",
+            "SUPINO",
+            "ROSCA"});
+            this.cbbExrcicios.Location = new System.Drawing.Point(12, 12);
+            this.cbbExrcicios.Name = "cbbExrcicios";
+            this.cbbExrcicios.Size = new System.Drawing.Size(225, 21);
+            this.cbbExrcicios.TabIndex = 0;
+            // 
+            // btnIF
+            // 
+            this.btnIF.Location = new System.Drawing.Point(12, 39);
+            this.btnIF.Name = "btnIF";
+            this.btnIF.Size = new System.Drawing.Size(75, 23);
+            this.btnIF.TabIndex = 1;
+            this.btnIF.Text = "IF";
+            this.btnIF.UseVisualStyleBackColor = true;
+            this.btnIF.Click += new System.EventHandler(this.btnIF_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(12, 68);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 2;
+            this.btnSwitch.Text = "SWITCH";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 100);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnIF);
+            this.Controls.Add(this.cbbExrcicios);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.ComboBox cbbExrcicios;
+        private System.Windows.Forms.Button btnIF;
+        private System.Windows.Forms.Button btnSwitch;
+    }
+}
+
---
+++ AppTeoria/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTeoria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIF_Click(object sender, EventArgs e)
+        {
+            //Aqui iremos utilizar o IF
+            //Como método validação
+            //Portanto iremos validar com base no texto
+            //da opção selecionada
+
+            //Utilizamos quando não temos uma variação
+            //do mesmo dado
+
+            //Criar um variavel para armazenar
+            //o resutado da opção
+
+            string opcao;
+
+            if (cbbExrcicios.Text == "LEGPRESS")
+                opcao = "Opção LEGPRESS";
+            else if (cbbExrcicios.Text == "ADUTORA")
+                opcao = "Opção ADUTORA";
+            else if (cbbExrcicios.Text == "ABDUTORA")
+                opcao = "Opção ABDUTORA";
+            else if (cbbExrcicios.Text == "SUPINO")
+                opcao = "Opção SUPINO";
+            else if (cbbExrcicios.Text == "ROSCA")
+                opcao = "Opção ROSCA";
+            else
+                opcao = "Opção Desconhecida";
+
+            MessageBox.Show(opcao);
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            //Aqui a validação se pela variação do 
+            //tipo de dado e não pelo texto
+
+            //Ou seja aqui estou olhando diretamente
+            //para a lista de opções
+
+            string opcao;
+
+            //SelectedIndex é a posição do item
+            //selecionado no array
+            switch (cbbExrcicios.SelectedIndex)
+            {
+                case 0: opcao = "Opção 1"; break;
+                case 1: opcao = "Opção 2"; break;
+                case 2: opcao = "Opção 3"; break;
+                case 3: opcao = "Opção 4"; break;
+                case 4: opcao = "Opção 5"; break;
+                default: opcao = "Opção Desconhecida"; break;
+            }
+
+            MessageBox.Show(opcao);
+        }
+    }
+}
