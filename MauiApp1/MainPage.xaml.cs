a97c1f58cf17c8687d37e300b3a9ad7efb4abcaf
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-05-05 19:52:41 -0300

    AppCaraCoroa

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using PCLExt.FileStorage.Folders;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Random sorteio = new Random();
+            int resultado = sorteio.Next(0, 1);
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
