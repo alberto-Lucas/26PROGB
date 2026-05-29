8e0f3467aba3435127de51a242cf7c40fedc9500
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-05-29 20:56:21 -0300

    AppCadastros_Cadastro

--- AppCadastro/frmCadCliente.Designer.cs
+++ AppCadastro/frmCadCliente.Designer.cs
@@ -114,6 +114,7 @@
             this.btnCancelar.TabIndex = 8;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnSalvar
             // 
--- AppCadastro/frmCadCliente.cs
+++ AppCadastro/frmCadCliente.cs
@@ -119,9 +119,132 @@ namespace AppCadastro
             File.WriteAllText(Caminho, Conteudo);
         }
 
+        //Função que retorna o caminho do arquivo completo
+        string GetDirArquivo(string TipoCadastro, string NomeArquivo)
+        {
+            //O caminho será montado:
+            //Diretorio Raiz do executavel
+            //Tipo do Cadastro: Cliente
+            //Nome Arquivo: CPF
+            //Ex: C:/Programas/AppCadastros/Cliente/00000000000.txt
+
+            //Diretorio Raiz
+            //AppDomain = O Executavel em si
+            //CurrentDomain = O processo em execução (gerenciador de tarefas)
+            //BaseDirectory = local da onde o .exe está sendo executado
+            string raiz = AppDomain.CurrentDomain.BaseDirectory;
+
+            //Montar o diretorio
+            return Path.Combine(
+                raiz, TipoCadastro, NomeArquivo + ".txt");
+
+            //OBS: A extensão é opicional como tambem o uso 
+            //de arquivo de texto, pois o sistema ira ler o arquivo 
+            //bruto, a extensão não tem impacto
+            //mudar ou não usar extensão dificulta o uso de 
+            //engenharia reversa para roubo de dados
+        }
+
+        //Funação de montagem do conteudo do arquivo
+        string GetCadastro()
+        {
+            //Iremos concatenar a informação da tela
+            //em uma unica string
+            //ja no formato de cadastro
+            //assim salvando tudo no arquivo
+            //Cada linha do arquivo será um campo
+
+            string cadastro =
+                "CPF=" + txtCPF.Text +
+                Environment.NewLine + //Quebra de Linha
+                "NOME=" + txtNome.Text +
+                Environment.NewLine +
+                "DTNASC=" + dtpDtNascimento.Value +
+                Environment.NewLine +
+                "RG=" + txtRG.Text;
+
+            return cadastro;
+        }
+
+        //Método Principal
+        //Salvar o cadastro, reuni todos os demais método e funções
+        void Salvar()
+        {
+            //Primeiro precisamos montar/recuperar
+            //o diretorio do arquivo
+
+            string diretorio = GetDirArquivo("Cliente", txtCPF.Text);
+
+            //Validar a existencia do arquivo
+            //se ja existir quer dizer que possui um cadasrto
+            try
+            {
+                //Validacao
+                if (ArquivoExiste(diretorio))
+                {
+                    MsgAlerta("Já existe um cadastro com este CPF.");
+                    return; //abortar
+                }
+            }
+            catch (Exception ex)
+            {
+                MsgErro("Falha ao validar o arquivo.", ex);
+                return; //Abortar a rotina
+            }
+
+            //Parte de gravação do arquivo
+            try
+            {
+                //Gravar o cadastro
+                GravarArquivo(diretorio, GetCadastro());
+
+                //Notificar o usuario
+                MsgInformacao("Cadastro realizado com sucesso!");
+
+                //Apenas para caracter de TESTE
+                //iremos exibir uma mensagem informando onde o arquivo 
+                //foi salvo
+                //Esta mensagem não deve ir na versão final do sistema
+                MsgInformacao("Salvo em:" + diretorio);
+            }
+            catch (Exception ex)
+            {
+                MsgErro("Falha ao salvar o cadastro.", ex);
+                //Não precisa do return para abortar por ser o final da rotina
+            }
+        }
+
+
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            Salvar();
 
+            //Limpar os campos
+            txtCPF.Clear();
+            txtNome.Clear();
+            txtRG.Clear();
+
+            //Unico diferente é o campo de data
+            //pois iremos resetar a data, usando a data atual
+            //DateTime componente de Data e Hora
+            //Now retorna a data e hora atual do computador
+            dtpDtNascimento.Value = DateTime.Now;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            //Validação se realmente deseja cancelar
+            bool resultado =
+                MessageBox.Show("Deseja realmente cancelar?",
+                                "Confirmação",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Question)
+                                //Validando se foi clicado no botão sim
+                                == DialogResult.Yes;
+            if (resultado)
+                this.Close(); //fechamos a tela
+
+            //Se clicar em não, não realizamos nenhuma ação
         }
     }
 }
